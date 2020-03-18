4fc71e3bb346ed3ab1471195c10f3f38b2860964
Fernando <fernandinhokhoury@hotmail.com>
2020-03-18 10:05:07 -0300

    Carregando dados da API

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
-export default function Repository({ match }) {
-    return (
-        <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>
-    );
+export default class Repository extends Component {
+    state = {
+        repository: {},
+        issues: [],
+        loading: true,
+    };
+
+    async componentDidMount() {
+        const { match } = this.props;
+
+        const repoName = decodeURIComponent(match.params.repository);
+
+        // api.github.com/repos/rocketseat/unform
+
+        const [repository, issues] = await Promise.all([
+            api.get(`/repos/${repoName}`),
+            api.get(`/repos/${repoName}/issues`, {
+                params: { // Opções de retorno
+                    state: 'open', // Somente as que estão abertas
+                    per_page: 5, // Quantidade de retornos
+                },
+            }),
+        ]);
+
+        this.setState({
+            repository: repository.data,
+            issues: issues.data,
+            loading: false,
+        })
+    }
+
+    render() {
+        const { repository, issues, loading } = this.state;
+
+        return <h1>Repository</h1>;
+    }
 }
